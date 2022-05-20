430853072e1b70f9a2c75ed870de48393007c8e3
Bomi-kwon <bbomicoding@gmail.com>
2022-05-21 00:31:42 +0900

    게시물 작성 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -9,11 +9,43 @@ public class Main {
 		System.out.println("==프로그램 시작==");
 
 		Scanner sc = new Scanner(System.in);
+		int 마지막_번호;
+		int number = 0;
 
-		
 		while (true) {
+			System.out.println("명령어 ) ");
+			String command = sc.nextLine();
+
+			if (command.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			} 
 			
+			else if (command.length() == 0) {
+				System.out.println("명령어를 입력해주세요.");
+				continue;
+			}
+			
+			else if (command.equals("article write")) {
+
+				System.out.println("제목 : ");
+				String x = sc.nextLine();
+				System.out.println("내용 : ");
+				String y = sc.nextLine();
+				마지막_번호 = number + 1;
+				System.out.printf("%d번글이 생성되었습니다.\n", 마지막_번호);
+				number = 마지막_번호;
+
+			}
+
+			else if (command.equals("System exit")) {
+				break;
+			}
+
+			else {
+				System.out.println("존재하지 않는 명령어입니다.");
+			}
 		}
+		sc.close();
+		System.out.println("==프로그램 끝==");
 	}
 }
-
