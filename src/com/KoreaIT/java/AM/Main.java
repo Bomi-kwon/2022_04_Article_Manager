e69bb493d23bcbece0dd8f5646c94615e50aaadc
Bomi-kwon <bbomicoding@gmail.com>
2022-05-21 01:04:58 +0900

    게시물 목록 기능 구현하기

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -10,15 +10,24 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		int 마지막_번호;
-		int number = 0;
+		int id = 0;
 
+		
+		List<Article> articles = new ArrayList<>();
+		
+		
 		while (true) {
 			System.out.println("명령어 ) ");
-			String command = sc.nextLine();
+			String command = sc.nextLine().trim();
 
 			if (command.equals("article list")) {
+				if (articles.size() == 0) {
 				System.out.println("게시글이 없습니다.");
-			} 
+				continue;
+			} else {
+				System.out.println("게시글이 있습니다.");
+			}
+			}
 			
 			else if (command.length() == 0) {
 				System.out.println("명령어를 입력해주세요.");
@@ -28,13 +37,15 @@ public class Main {
 			else if (command.equals("article write")) {
 
 				System.out.println("제목 : ");
-				String x = sc.nextLine();
+				String title = sc.nextLine();
 				System.out.println("내용 : ");
-				String y = sc.nextLine();
-				마지막_번호 = number + 1;
+				String body = sc.nextLine();
+				마지막_번호 = id + 1;
 				System.out.printf("%d번글이 생성되었습니다.\n", 마지막_번호);
-				number = 마지막_번호;
+				id = 마지막_번호;
 
+				Article article = new Article(id, title, body);
+				articles.add(article);
 			}
 
 			else if (command.equals("System exit")) {
@@ -47,5 +58,17 @@ public class Main {
 		}
 		sc.close();
 		System.out.println("==프로그램 끝==");
+	}
+}
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		
 	}
 }
